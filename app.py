c9108cb181d67cdbea6a3e8364eaa83ab687e2ed
Jordan-Fakers <Jordan.j.talvin@gmail.com>
2021-03-07 14:40:31 +0100

    preparation de la fonction mail + user

--- app.py
+++ app.py
@@ -4,28 +4,21 @@ from flask import Flask,render_template,url_for,request,redirect,jsonify
 import logging
 import datetime
 import locale
-from bdd import Mybdd
+from bdd import MyDB
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/wiki')
 def wiki():
-    result_dict = []
     cursor = mybdd.cursor()
     sql = "SELECT * FROM articles" 
     cursor.execute(sql)
     app.logger.info('%s Database successfully connected')
-    # for res in cursor:
-    #     result_dict.append({'id':res['id'],'title':res['title'], 'images':res['image'], 'content':res['content'], 'date':res['date']})
     results = cursor.fetchall()
     app.logger.info('%s database successfully filled')
     print(results)
     # return jsonify(result_dict)
-    return render_template('index.html',len=len(results))
+    return render_template('index.html',len=len(results), results=results)
     
 
 # @app.route('/mail')
